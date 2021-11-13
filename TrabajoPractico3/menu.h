0e50b8a3b8f6cf7c3c64968ee89907c35b2f0beb
Carolina Sotelo <carolinasotelo97@gmail.com>
2021-11-13 16:36:34 -0300

    TP3

---
+++ TrabajoPractico3/menu.h
@@ -0,0 +1,34 @@
+/*
+ * menu.h
+ *
+ *  Created on: 18 jun. 2021
+ *      Author: caroo
+ */
+
+#ifndef MENU_H_
+#define MENU_H_
+
+
+
+#endif /* MENU_H_ */
+
+/** \brief muestra al usuario un menu de opciones y le pide al usuario que elija una
+ *
+ * \return int La opcion elegida
+ *
+ */
+int menu();
+
+/** \brief muestra opciones de modificaciones y le pide al usuario que elija una
+ *
+ * \return char La opcion elegida
+ *
+ */
+char submenuModificar();
+
+/** \brief muestra un menu de opciones con respecto al orden y le pide al usuario que elija una
+ *
+ * \return char La opcion elegida
+ *
+ */
+char submenuOrdenar();
